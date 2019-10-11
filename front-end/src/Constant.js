ae6c15afbf5e530cfdc3bf7e41bf6dfbc69ced42
Conor Muldoon <conor.muldoon@gmail.com>
2019-10-11 16:51:39 +0100

    Hard coding CSV domain as proxy works for fetch, but not anchors

--- front-end/src/Constant.js
+++ front-end/src/Constant.js
@@ -3,6 +3,7 @@ export const FORMAT = "D/M/YYYY";
 
 export const API = "";
 //export const API = "/apiacc";
+export const CSV_DOMAIN = "http://localhost:8080";
 
 export const LPER = "20\u1d57\u02b0 percentile";
 export const HPER = "80\u1d57\u02b0 percentile";
