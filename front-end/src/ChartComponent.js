2e4525435e1b2c0d757b94139b3a9f958abe3107
Conor Muldoon <conor.muldoon@gmail.com>
2019-10-11 19:48:53 +0100

    Using lodash to check object properties in ChartComponent when switching between weather variables

--- front-end/src/ChartComponent.js
+++ front-end/src/ChartComponent.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import {VictoryChart,VictoryLine,VictoryTheme,VictoryAxis,VictoryZoomContainer} from 'victory';
-
+import {has} from 'lodash';
 import PropTypes from 'prop-types';
 
 import './App.css';
+import {varMapping} from './Util';
+
+
+const PERCENT="Percentage";
+const CELSIUS="Celsius";
+
+const PREVAL="precipitation.value";
 
 /**
  * A component for displaying line graphs.
@@ -11,37 +18,108 @@ import './App.css';
  */
 export default function ChartComponent(props) {
 
+  const [yVal,setYVal]=useState(undefined);
+  const [yLabel,setYLabel]=useState(undefined);
 
-    return (
 
-      <VictoryChart
-        theme={VictoryTheme.material}
-        scale={{ x: "time" }}
-        containerComponent={
-            <VictoryZoomContainer
-              zoomDimension="x"
-              zoomDomain={props.zoomDomain}
-              onZoomDomainChange={props.handleZoom}
-            />
+  useEffect(()=>{
+
+    
+
+      // 'Precipitation','Humidity','Wind Direction','Wind Speed','Cloudiness','Cloud Level','Dew Point','Pressure','Temperature'
+      const variRequest=varMapping(props.varCur);
+
+      if(variRequest === "precipitation"){
+        if(props.minMax){
+          if(props.index===0){
+            setYVal("precipitation.minvalue");
+          }else if(props.index===1){
+            setYVal(PREVAL);
+          }else{
+            setYVal("precipitation.maxvalue");
           }
-      >
-        {props.data&&<VictoryLine
-          style={{
-            data: { stroke: "#c43a31" },
-            parent: { border: "1px solid #ccc"}
-          }}
-          data={props.data}
-          x="date"
-          y={props.yVal}
-        />}
+  
+        }else{
+          setYVal(PREVAL);
+        }
+        setYLabel("Millimetres");
+  
+      }else if(variRequest === "humidity"){
+        setYVal(variRequest);
+        setYLabel(PERCENT);
+      }else if(variRequest === "windDirection"){
+  
+        // Displaying sin of the angle on the graph, rather than the angle/
+        setYVal("windDirection.sinDeg");
+        setYLabel("sin of angle");
+      }else if(variRequest === "windSpeed"){
+        setYVal("windSpeed.mps");
+        setYLabel("Metres per second");
+      }else if(variRequest === "cloudiness"){
+        setYVal(variRequest);
+        setYLabel(PERCENT);
+      }else if(variRequest === "cloudLevel"){
+        if(props.index===0){
+          setYVal("cloud.low");
+        }else if(props.index===1){
+          setYVal("cloud.medium");
+        }else{
+          setYVal("cloud.high");
+        }
+        setYLabel(PERCENT);
+      }else if(variRequest === "dewPoint"){
+        setYVal(variRequest);
+        setYLabel(CELSIUS);
+      }else if(variRequest === "pressure"){
+        setYVal(variRequest);
+        setYLabel("Hectopascals");
+      }else if(variRequest === "temperature"){
+        setYVal(variRequest);
+        setYLabel(CELSIUS);
+      }
+      
+    
+  },[props.varCur,props.minMax,props.index]);
+    
 
-        {props.data&&<VictoryAxis fixLabelOverlap={true}/>}
-        {props.data&&<VictoryAxis dependentAxis label={props.yLabel} style={{ axisLabel: {padding: 45} }} tickFormat={
-                (x) => {if(x<.001){return x.toFixed(3);} if(x>999){return Math.round(x);} return x;}} />}
+    
 
-      </VictoryChart>
+    if(yVal&&props.data&&has(props.data[0],yVal)){
 
-    );
+      return (
+
+        <VictoryChart
+          theme={VictoryTheme.material}
+          scale={{ x: "time" }}
+          containerComponent={
+              <VictoryZoomContainer
+                zoomDimension="x"
+                zoomDomain={props.zoomDomain}
+                onZoomDomainChange={props.handleZoom}
+              />
+            }
+        >
+          {props.data&&<VictoryLine
+            style={{
+              data: { stroke: "#c43a31" },
+              parent: { border: "1px solid #ccc"}
+            }}
+            data={props.data}
+            x="date"
+            y={yVal}
+          />}
+
+          {props.data&&<VictoryAxis fixLabelOverlap={true}/>}
+          {props.data&&<VictoryAxis dependentAxis label={yLabel} style={{ axisLabel: {padding: 45} }} tickFormat={
+                  (x) => {if(x<.001){return x.toFixed(3);} if(x>999){return Math.round(x);} return x;}} />}
+
+        </VictoryChart>
+
+        );
+      }else{
+
+        return false;
+      }
 
 }
 
--- front-end/src/TabsComponent.js
+++ front-end/src/TabsComponent.js
@@ -4,13 +4,10 @@ import "react-tabs/style/react-tabs.css";
 import ChartComponent from './ChartComponent';
 
 import {LPER,MEDIAN,HPER,LOW,MEDIUM,HIGH,CL} from './Constant';
-import {varMapping} from './Util';
+
 import PropTypes from 'prop-types';
 
-const PERCENT="Percentage";
-const CELSIUS="Celsius";
 
-const PREVAL="precipitation.value";
 
 
 
@@ -20,104 +17,54 @@ const PREVAL="precipitation.value";
  * only a single ChartComponent is displayed.
  *  
  */
+
 export default function TabsComponent(props){
 
   const [tabs,setTabs]=useState(undefined);
 
-  const [yVal,setYVal]=useState(undefined);
-  const [yLabel,setYLabel]=useState(undefined);
-
 
   useEffect(()=>{
 
-    function determineLabel(variRequest){
-
-      // 'Precipitation','Humidity','Wind Direction','Wind Speed','Cloudiness','Cloud Level','Dew Point','Pressure','Temperature'
+    function updateTabs(){
+      if(props.minMax){
   
-      if(variRequest === "precipitation"){
-        if(props.minMax){
-          setYVal(["precipitation.minvalue",PREVAL,"precipitation.maxvalue"]);
-  
-        }else{
-          setYVal(PREVAL);
-        }
-        setYLabel("Millimetres");
-  
-      }else if(variRequest === "humidity"){
-        setYVal(variRequest);
-        setYLabel(PERCENT);
-      }else if(variRequest === "windDirection"){
-  
-        // Displaying sin of the angle on the graph, rather than the angle/
-        setYVal("windDirection.sinDeg");
-        setYLabel("sin of angle");
-      }else if(variRequest === "windSpeed"){
-        setYVal("windSpeed.mps");
-        setYLabel("Metres per second");
-      }else if(variRequest === "cloudiness"){
-        setYVal(variRequest);
-        setYLabel(PERCENT);
-      }else if(variRequest === "cloudLevel"){
-        setYVal(["cloud.low","cloud.medium","cloud.high"])
-        setYLabel(PERCENT);
-      }else if(variRequest === "dewPoint"){
-        setYVal(variRequest);
-        setYLabel(CELSIUS);
-      }else if(variRequest === "pressure"){
-        setYVal(variRequest);
-        setYLabel("Hectopascals");
-      }else if(variRequest === "temperature"){
-        setYVal(variRequest);
-        setYLabel(CELSIUS);
+        setTabs([LPER,MEDIAN,HPER]);
+    
+      }else if(props.varCur===CL){
+    
+        setTabs([LOW,MEDIUM,HIGH]);
+    
+      }else{
+        setTabs(undefined);
       }
-      
     }
 
     if(props.data&&props.data.length>0){
 
-        const variRequest=varMapping(props.varCur);
-        determineLabel(variRequest);
-        if(props.minMax){
-  
-          setTabs([LPER,MEDIAN,HPER]);
-      
-        }else if(props.varCur===CL){
-      
-          setTabs([LOW,MEDIUM,HIGH]);
-      
-        }else if(tabs){
-          setTabs(undefined);
-        }
-        
+        updateTabs(props.minMax,props.varCur===CL);     
 
       }else{
         
-        setTabs(undefined);
-        setYVal(undefined);
-        setYLabel(undefined);
-        
+        setTabs(undefined);  
         
       }
-    },[props.data,props.varCur,props.minMax,tabs]
+    },[props.data,props.varCur,props.minMax]
   );
-  
-    
-
   
 
       function tabsPanel(index){
         return (<TabPanel >
           <div id="tabdiv" data-testid='chart' >
-            <ChartComponent data={props.data} zoomDomain={props.zoomDomain} yVal={yVal[index]}
+            <ChartComponent data={props.data} zoomDomain={props.zoomDomain} index={index}
               handleZoom={props.setZoomDomain}
-            yLabel={yLabel} />
+              varCur={props.varCur} minMax={props.minMax}  />
           </div>
         </TabPanel>
         );
       }
 
       return (<div  id="chartdiv">
-          {tabs&&yVal&&<Tabs id="tabscomponent" defaultIndex={1}>
+          {tabs&&<Tabs id="tabscomponent" defaultIndex={1}>
           <TabList >
             <Tab>{tabs[0]}</Tab>
             <Tab>{tabs[1]}</Tab>
@@ -130,12 +77,11 @@ export default function TabsComponent(props){
 
         </Tabs>}
 
-        {yVal&&!tabs&&<div data-testid='chart' id="singlevar">
+        {!tabs&&<div data-testid='chart' id="singlevar">
           <ChartComponent data={props.data} zoomDomain={props.zoomDomain} handleZoom={props.setZoomDomain}
-            yVal={yVal} yLabel={yLabel} />
+            varCur={props.varCur} minMax={props.minMax} />
           </div>
         }
-        {!yVal&&<div/>}
       </div>
     );
 
