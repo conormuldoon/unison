557fc53f95860c8fb4490c417a9387b86e530e60
Conor Muldoon <conor.muldoon@gmail.com>
2019-12-12 16:03:57 +0000

    Changed default configuration to use the open access Met Éireann server

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -7,10 +7,10 @@ import Unison from './Unison';
 
 // Map coordinates
 // Dublin
-//const mapCentre=[53.35014, -6.266155];
+const mapCentre=[53.35014, -6.266155];
 
 // Oslo
-const mapCentre = [59.922326, 10.751560];
+//const mapCentre = [59.922326, 10.751560];
 
 ReactDOM.render(<Unison mapCentre={mapCentre} logoLeft={require('./Acclimatize-Logo.png')} logoRight={require('./partners-logos.jpg')} />, document.getElementById('root'));
 registerServiceWorker();
