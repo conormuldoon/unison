d819fae708c1b743a1f46a96b5aa33766ba6ea14
Conor Muldoon <conor.muldoon@gmail.com>
2020-04-09 15:01:34 +0100

    Added document request exception and harvester configruation

---
+++ back-end/src/main/java/eu/acclimatize/unison/harvester/DocumentRequestException.java
@@ -0,0 +1,14 @@
+package eu.acclimatize.unison.harvester;
+
+public class DocumentRequestException extends Exception{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7544527923123881454L;
+
+	public DocumentRequestException(String message){
+		super(message);
+	}
+
+}
