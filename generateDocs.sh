48402655c5e781dde412c61d1f3ef3dabf91ee85
Conor Muldoon <conor.muldoon@gmail.com>
2020-04-10 18:50:00 +0100

    Updated scripts

--- generateDocs.sh
+++ generateDocs.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-rm -r docs/back-end/*
-rm -r docs/front-end/*
+rm -rf docs/
+mkdir -p docs/back-end
 cd back-end
 mvn javadoc:javadoc
 mv target/site/apidocs/* ../docs/back-end
 cd ../front-end
+mkdir ../docs/front-end
 yarn docs
 mv docs/* ../docs/front-end
