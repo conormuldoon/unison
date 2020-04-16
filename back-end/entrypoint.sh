7e6769543bc6af68227f30cd226a1184633110c2
Conor Muldoon <conor.muldoon@gmail.com>
2020-04-16 12:36:35 +0100

    Edited the back-end/entrypoint.sh script and incremented the version number

--- back-end/entrypoint.sh
+++ back-end/entrypoint.sh
@@ -7,8 +7,7 @@ until PGPASSWORD=$POSTGRES_PASSWORD psql -h $1 -U "$POSTGRES_USER" -c '\q'; do
   sleep 5
 done
   
->&2 echo "Harmonie database is ready to receive connections."
-shift
+echo "Harmonie database is ready to receive connections."
 exec java -cp app:app/lib/* -Ddefault.username=unisonuser -Ddefault.encoded=$2a$10$A2tDbzPnFa0ti2z5Da/HNeSlxVL2MC8fLK1AWXbphOI1Vo6S8swGC eu.acclimatize.unison.UnisonServerApplication
 
 
