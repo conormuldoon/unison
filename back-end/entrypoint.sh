4172a43c03de725f25e012ecbc7df62426872726
Conor Muldoon <conor.muldoon@gmail.com>
2020-04-17 16:48:27 +0100

    Using environment varialbes for default user name and encoded password.

--- back-end/entrypoint.sh
+++ back-end/entrypoint.sh
@@ -2,12 +2,14 @@
 # A script executed by the Docker container that checks whether the harmonie database is ready prior to launching the Spring Boot application.
 set -e
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h $DB_HOST -U "$POSTGRES_USER" -c '\q'; do
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h $UNISON_DB_HOST -U $POSTGRES_USER -c '\q'; do
   >&2 echo "Harmonie database is not ready - sleeping."
   sleep 5
 done
   
 echo "Harmonie database is ready to receive connections."
-exec java -cp app:app/lib/* -Ddefault.username=unisonuser -Ddefault.encoded=$2a$10$A2tDbzPnFa0ti2z5Da/HNeSlxVL2MC8fLK1AWXbphOI1Vo6S8swGC eu.acclimatize.unison.UnisonServerApplication
+
+exec java -cp app:app/lib/* -Ddefault.username=$UNISON_USER -Ddefault.encoded=$UNISON_ENCODED eu.acclimatize.unison.UnisonServerApplication
+
 
 
