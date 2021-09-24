f4525f44c1060c1858b94f018aebc895cdd26628
Conor Muldoon <conor.muldoon@gmail.com>
2021-09-24 14:57:16 +0100

    Added Javadoc for the DTOs

--- back-end/src/main/java/eu/acclimatize/unison/location/LocationDTO.java
+++ back-end/src/main/java/eu/acclimatize/unison/location/LocationDTO.java
@@ -4,6 +4,12 @@ import org.locationtech.jts.geom.Point;
 
 import eu.acclimatize.unison.user.UserInformation;
 
+/**
+ * A DTO for receiving location data. Data must be sent by the client in a
+ * GeoJSON format. The GeoJSON is deserialized by the
+ * {@link LocationDeserializer}.
+ *
+ */
 public class LocationDTO {
 
 	private String name;
@@ -12,12 +18,28 @@ public class LocationDTO {
 
 	private Point geom;
 
+	/**
+	 * Creates an instance of LocationDTO.
+	 * 
+	 * @param name The name of the location.
+	 * @param user The information of the user that added the location. When
+	 *             creating an instance of the DTO the {@link LocationDeserializer}
+	 *             uses the database and the name of the authenticated user to
+	 *             obtain the user information, as the user information is not
+	 *             transmitted in the request body by the client.
+	 * @param geom The point coordinates of the location.
+	 */
 	public LocationDTO(String name, UserInformation user, Point geom) {
 		this.name = name;
 		this.user = user;
 		this.geom = geom;
 	}
 
+	/**
+	 * Creates a JPA entity for the location.
+	 * 
+	 * @return The entity created.
+	 */
 	public Location createEntity() {
 		return new Location(name, user, geom);
 	}
--- back-end/src/main/java/eu/acclimatize/unison/user/UserInfoDTO.java
+++ back-end/src/main/java/eu/acclimatize/unison/user/UserInfoDTO.java
@@ -3,6 +3,13 @@ package eu.acclimatize.unison.user;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
+/**
+ * 
+ * A DTO for receiving user information. The password must be bcrypt encoded
+ * prior to being transferred to the server. Jackson is used to deserialize the
+ * received JSON and construct the DTO instance.
+ *
+ */
 public class UserInfoDTO {
 
 	@JsonProperty
@@ -11,6 +18,11 @@ public class UserInfoDTO {
 	@JsonProperty(access = Access.WRITE_ONLY)
 	private String encodedPassword;
 
+	/**
+	 * Creates a JPA entity for the user information.
+	 * 
+	 * @return The created entity.
+	 */
 	public UserInformation createEntity() {
 		return new UserInformation(userName, encodedPassword);
 	}
