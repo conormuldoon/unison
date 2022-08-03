7a291bea3c7bd8466a777681f413e8877ca3f557
Conor Muldoon <conor.muldoon@gmail.com>
2022-08-03 15:31:30 +0100

    Updated the end-point URI for the Norwegian Meteorological Institute and added the User-Agent header to requests, as it is required for Norwegian Meteorological Institute locationforecast end-point.

--- back-end/src/main/java/eu/acclimatize/unison/location/harvester/DocumentRequestService.java
+++ back-end/src/main/java/eu/acclimatize/unison/location/harvester/DocumentRequestService.java
@@ -2,6 +2,8 @@ package eu.acclimatize.unison.location.harvester;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
 
 import javax.xml.parsers.DocumentBuilder;
 
@@ -14,12 +16,14 @@ import eu.acclimatize.unison.location.Location;
 
 /**
  * 
- * A service that obtains XML weather data documents from a HARMONIE-AROME endpoint
- * for the coordinates of locations.
+ * A service that obtains XML weather data documents from a HARMONIE-AROME
+ * endpoint for the coordinates of locations.
  *
  */
 @Service
 public class DocumentRequestService {
+
+	private static final String UAS = "java/" + System.getProperty("java.version");
 
 	private DocumentBuilder documentBuilder;
 	private String template;
@@ -28,8 +32,8 @@ public class DocumentRequestService {
 	 * Creates an instance of DocumentRequestService.
 	 * 
 	 * @param documentBuilder
-	 * @param template The template for a HARMONIE-AROME endpoint specified by
-	 *                        app.uri in the application properties file.
+	 * @param template        The template for a HARMONIE-AROME endpoint specified
+	 *                        by app.uri in the application properties file.
 	 */
 	public DocumentRequestService(DocumentBuilder documentBuilder, @Value("${harmonie.uri}") String template) {
 
@@ -43,9 +47,12 @@ public class DocumentRequestService {
 	 * @param location The location used in obtaining data.
 	 * @return The requested document.
 	 * 
-	 * @throws HarvestParseException Thrown if there was a SAX exception when parsing.
-	 * @throws HarvestRequestException Thrown if there was an I/O exception when connecting to the URI.
-	 * @throws DocumentNotFoundException Thrown if there was no XML document found at the URI.
+	 * @throws HarvestParseException     Thrown if there was a SAX exception when
+	 *                                   parsing.
+	 * @throws HarvestRequestException   Thrown if there was an I/O exception when
+	 *                                   connecting to the URI.
+	 * @throws DocumentNotFoundException Thrown if there was no XML document found
+	 *                                   at the URI.
 	 * 
 	 */
 	public Document documentForLocation(Location location)
@@ -54,12 +61,16 @@ public class DocumentRequestService {
 		String locationURI = location.replaceVariables(template);
 
 		try {
-			return documentBuilder.parse(locationURI);
+			URL url = new URL(locationURI);
+			URLConnection connection = url.openConnection();
+			connection.setRequestProperty("User-Agent", UAS);
+			return documentBuilder.parse(connection.getInputStream());
 		} catch (FileNotFoundException e) {
 			throw new DocumentNotFoundException("No XML doucment was found at " + locationURI + ".");
 		} catch (SAXException e) {
 			throw new HarvestParseException("There was a SAX exception when parsing " + locationURI + ".");
 		} catch (IOException e) {
+			e.printStackTrace();
 			throw new HarvestRequestException("There was an I/O exception when connecting to " + locationURI + ".");
 		}
 
